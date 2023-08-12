06d999c1504bcb7bfef6f82648bc4135c51b15b7
user <oiehomail@gmail.com>
2023-08-12 15:20:14 +0900

    no message

--- src/main/java/com/oieho/config/CorsProperties.java
+++ src/main/java/com/oieho/config/CorsProperties.java
@@ -1,19 +1,19 @@
-package com.oieho.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Component
-@Getter
-@Setter
-@ConfigurationProperties(prefix = "cors")
-public class CorsProperties {
-    private String allowedOrigins;
-    private String allowedMethods;
-    private String allowedHeaders;
-    private Long maxAge;
-}
-
+//package com.oieho.config;
+//
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.stereotype.Component;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Component
+//@Getter
+//@Setter
+//@ConfigurationProperties(prefix = "cors")
+//public class CorsProperties {
+//    private String allowedOrigins;
+//    private String allowedMethods;
+//    private String allowedHeaders;
+//    private Long maxAge;
+//}
+//
