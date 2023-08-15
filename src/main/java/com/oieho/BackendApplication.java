7c0d1e5d3d7166c5a62a4dd64b54c84fd8e6ddb4
Laptop\User <oiehomail@gmail.com>
2023-08-15 18:39:56 +0900

    no message

--- src/main/java/com/oieho/BackendApplication.java
+++ src/main/java/com/oieho/BackendApplication.java
@@ -22,7 +22,7 @@ public class BackendApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOriginPatterns("*").allowedMethods("*");
+                registry.addMapping("/**").allowedOriginPatterns("*").allowedMethods("*").allowedHeaders("*");
             }
         };
     }
