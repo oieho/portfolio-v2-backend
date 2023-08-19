aed78609362846af08d952539faae3f247095d65
Laptop\User <oiehomail@gmail.com>
2023-08-19 15:44:42 +0900

    no message

--- src/main/java/com/oieho/oauth/handler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/oieho/oauth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -111,7 +111,7 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 
 		response.addHeader(SecurityConstants.TOKEN_HEADER, SecurityConstants.TOKEN_PREFIX + accessToken);
 		response.addHeader(SecurityConstants.REFRESH_HEADER, SecurityConstants.REFRESH_PREFIX + refreshToken);
-        return UriComponentsBuilder.fromUriString(targetUrl+"socialLogin")
+        return UriComponentsBuilder.fromUriString(targetUrl+"/#/socialLogin/")
                 .queryParam("accessToken",accessToken)
                 .queryParam("refreshToken",refreshToken)
                 .build().toUriString();
