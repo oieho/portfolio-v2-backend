36c0f4269c07d16b32fa31cae7bf8af22f34c320
Laptop\User <oiehomail@gmail.com>
2023-08-22 02:32:22 +0900

    no message

--- src/main/java/com/oieho/config/SecurityConfig.java
+++ src/main/java/com/oieho/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
 	public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("https://oieho.xyz");
+        configuration.addAllowedOrigin("http://oieho.xyz");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
