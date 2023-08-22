51c4027faae00a125c914a2a1344d991dd9f108a
Laptop\User <oiehomail@gmail.com>
2023-08-22 19:58:12 +0900

    no message

--- src/main/java/com/oieho/oauth/handler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/oieho/oauth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -111,7 +111,7 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 
 		response.addHeader(SecurityConstants.TOKEN_HEADER, SecurityConstants.TOKEN_PREFIX + accessToken);
 		response.addHeader(SecurityConstants.REFRESH_HEADER, SecurityConstants.REFRESH_PREFIX + refreshToken);
-        return UriComponentsBuilder.fromUriString("/")
+        return UriComponentsBuilder.fromUriString(targetUrl)
                 .queryParam("accessToken",accessToken)
                 .queryParam("refreshToken",refreshToken)
                 .build().toUriString();
