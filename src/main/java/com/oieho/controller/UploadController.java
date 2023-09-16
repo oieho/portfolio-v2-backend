d02fe20b3bc4030964919627a3e0de3a797c0f66
oieho <oiehomail@gmail.com>
2023-09-16 10:47:25 +0900

    no message

--- src/main/java/com/oieho/controller/UploadController.java
+++ src/main/java/com/oieho/controller/UploadController.java
@@ -136,7 +136,7 @@ public class UploadController {
 				break;
 			}
 			
-			String rootStr = "/static/" + "boardImgs";
+			String rootStr = "/" + "boardImgs";
 			fileData.put("url", rootStr + "/" + folderPath + "/" + uuid + "_" + originalName);
 			fileData.put("name", originalName);
 			fileData.put("size", uploadFile.getSize());
