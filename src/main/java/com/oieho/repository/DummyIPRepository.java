eabbe415aadc4f5c305a9ebeb6e1776fdb14fea1
oieho <oiehomail@gmail.com>
2023-09-18 11:33:26 +0900

    no message

--- src/main/java/com/oieho/repository/DummyIPRepository.java
+++
@@ -1,9 +0,0 @@
-package com.oieho.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.oieho.entity.DummyIP;
-
-public interface DummyIPRepository extends JpaRepository<DummyIP, String> {
-
-}
