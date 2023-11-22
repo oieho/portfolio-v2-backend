5c235786419ad9f74ae15b9c5a64672ec0c00969
oiehomail@gmail.com <oiehomail@gmail.com>
2023-11-22 13:14:35 +0900

    JWT Token 발행 Header 수정(새로고침시 로그인이 풀리는 버그와 DB에서 유효한 리프레시 토큰을 조회하지 못하는 문제 수정)

--- src/main/java/com/oieho/jwt/SecurityConstants.java
+++ src/main/java/com/oieho/jwt/SecurityConstants.java
@@ -5,6 +5,9 @@ public final class SecurityConstants {
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String REFRESH_HEADER = "RefreshAuthorization";
 	public static final String REFRESH_PREFIX = "Bearer ";
+	public static final String INVALID_ACCESS_TOKEN_HEADER = "InvalidAccessAuthorization";
+	public static final String INVALID_REFRESH_TOKEN_HEADER = "InvalidRefreshAuthorization";
+	public static final String INVALID_ALL_TOKEN_HEADER = "InvalidAllAuthorization";
 	
 	public static final String TOKEN_TYPE = "JWT";
 	public static final String AUTH_LOGIN_URL = "/auth/authenticate";
@@ -12,5 +15,4 @@ public final class SecurityConstants {
 	public static final String invalidAccessToken = "Invalid access token.";
 	public static final String invalidRefreshToken = "Invalid refresh token.";
 	public static final String invalidAllTokens  = "Invalid access and refresh tokens.";
-	public static final String notExpiredTokenYet = "Not expired token yet.";
 	} 
